3bea42fb4102f5401297a909fcaf3556640efb60
Julia-itcourse <juliaartemenko93@gmail.com>
2022-01-30 15:57:06 +0100

    fix: replaces burger with css animation

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-const burger = document.getElementById("burger");
-const navigationList = document.getElementById("navigation-list");
+// const burger = document.getElementById("burger");
+// const navigationList = document.getElementById("navigation-list");
 
-burger.addEventListener("click", () => {
-  navigationList.classList.toggle("navigation-list--open");
-  burger.classList.toggle("burger--close");
-});
+// burger.addEventListener("click", () => {
+//   navigationList.classList.toggle("navigation-list--open");
+//   burger.classList.toggle("burger--close");
+// });
 
-const navItems = document.querySelectorAll(".navigation__link");
-console.log(navItems);
+// const navItems = document.querySelectorAll(".navigation__link");
+// console.log(navItems);
 
-navItems.forEach((el) => {
-  el.addEventListener("click", () => {
-    navigationList.classList.remove("navigation-list--open");
+// navItems.forEach((el) => {
+//   el.addEventListener("click", () => {
+//     navigationList.classList.remove("navigation-list--open");
+//   });
+// });
+
+const refs = {
+  burger: document.getElementById("hamburger"),
+  menuWrap: document.getElementById("menu-wrap"),
+  navItems: document.querySelectorAll(".navigation-menu__link"),
+  menu: document.querySelector(".menu"),
+};
+
+const changeClass = (action, el, className) => {
+  if (action === "toggle") {
+    el.classList.toggle(className);
+  }
+  if (action === "remove") {
+    el.classList.remove(className);
+  }
+  if (action === "add") {
+    el.classList.add(className);
+  }
+};
+
+const openBurger = () => {
+  changeClass("toggle", refs.burger, "is-active");
+  changeClass("toggle", refs.menuWrap, "is-open");
+
+  refs.navItems.forEach((el) => {
+    el.addEventListener("click", () => {
+      changeClass("remove", refs.menuWrap, "is-open");
+      changeClass("remove", refs.burger, "is-active");
+    });
   });
-});
+};
+
+refs.burger.addEventListener("click", () => openBurger());
 
 scrollBtn = document.getElementById("scroll-btn");
 
@@ -22,10 +55,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollBtn.style.display = "block";
   } else {
     scrollBtn.style.display = "none";
